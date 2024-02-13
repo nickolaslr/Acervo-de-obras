ea3b3d0dc9f7562b6c0a17b75bc10b5a3761d634
Nickolas <nickolaslr@gmail.com>
2024-02-13 20:21:53 -0300

    Python

--- booksdb.py
+++ booksdb.py
@@ -1,11 +1,32 @@
 import sqlite3
 
-connection = sqlite3.connect('booksdb.db')
-cursor = connection.cursor()
 
-#--search the db--
-#by title
-#by author
+# -- search the db --
+# by title
 
-#--insert new itens on db--
-#title...
+conn = sqlite3.connect('booksdb.db')
+cursor = conn.cursor()
+
+search_value = input("Qual o nome da obra? ")
+# Lembrando que o input recebe uma string
+
+search_query = "SELECT * FROM books WHERE title = ?"
+
+cursor.execute(search_query, (search_value,))
+# Virgula para criar uma tupla
+
+results = cursor.fetchall()
+
+for row in results:
+    print(row)
+    #Melhorar o layout das informações exibidas
+    #Colocar not found se não encontrar
+
+cursor.close()
+conn.close()
+
+
+# -- search the db --
+# by author
+
+# -- insert new itens in the db --
